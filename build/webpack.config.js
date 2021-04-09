1d52f1c77b39d7cdba91228d34a2d3a18370e0a8
tuanteng96 <tuantengyn@gmail.com>
2021-04-09 10:29:22 +0700

    update

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         path: resolvePath('www'),
         filename: 'js/[name].js',
         chunkFilename: 'js/[name].js',
-        publicPath: 'https://cbeautyclinic.vn/app2021/',
+        publicPath: '',
         hotUpdateChunkFilename: 'hot/hot-update.js',
         hotUpdateMainFilename: 'hot/hot-update.json',
     },
